b613a5aec0a7fa001e9491acb495a62f40d543ca
Fabio Coelho <fabioolivc@gmail.com>
2022-08-08 08:10:13 -0300

    Adding task detail component

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,14 +1,27 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
 import { Task } from "../shared/task.model";
+import { TaskService } from "../shared/task.service";
 
 @Component({
   selector: 'task-detail',
   templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit{
   @Input() public task: Task | undefined;
 
-  public constructor(){}
+  public constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute
+  ){}
+
+  public ngOnInit(){
+    this.route.params
+      .subscribe((params: Params) => {
+        this.taskService.getTask(+params['id'])
+        .then(task => this.task = task)
+      })
+  }
 }
