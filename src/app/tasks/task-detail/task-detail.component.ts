a1032b614b7abb31746c3f3858e3333a2207a205
Fabio Coelho <fabioolivc@gmail.com>
2022-08-21 15:10:56 -0300

    implement betters to form responsive

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -29,6 +29,7 @@ export class TaskDetailComponent implements OnInit, AfterViewInit{
     private location: Location
   ){ 
     this.reactiveTaskForm = new FormGroup({
+      id: new FormControl(null),
       title: new FormControl(null),
       deadline: new FormControl(null),
       done: new FormControl(null),
@@ -38,14 +39,29 @@ export class TaskDetailComponent implements OnInit, AfterViewInit{
 
 
   public ngOnInit(){
+    this.task = new Task(null, null )
+
     this.route.params
     .pipe(switchMap((params: Params) =>  this.taskService.getById(+params['id'])))
     .subscribe(
-      task => this.task = task,
+      task => this.setTask(task as Task),
       Error => alert("Ocorreu um erro no servidor, por favor tente mais tarde.")
       )
     
   }
+  public setTask(task: Task): void{
+    this.task = task;
+
+    // setValue
+    let formModel ={
+      title: task.title || null,
+      deadline: task.deadline || null,
+      description: task.description || null,
+      done: task.done || null
+    }
+    this.reactiveTaskForm.setValue(formModel);
+  }
+
   ngAfterViewInit() {
     $("#exemplo").fadeOut(9999)
   }
