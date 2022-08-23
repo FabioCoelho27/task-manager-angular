5eb05de89c824ee704b7de74dc4812d4b0f53e5f
Fabio Coelho <fabioolivc@gmail.com>
2022-08-23 20:37:07 -0300

    Implementing searchById

--- src/app/in-memory-task-data.service.ts
+++ src/app/in-memory-task-data.service.ts
@@ -8,13 +8,13 @@ import { Observable } from "rxjs";
 export class InMemoryTaskDataService implements InMemoryDbService{
   public createDb(){
     let tasks = [
-      { id: 1, title: 'Fazer tarefa 1'},
-      { id: 2, title: 'Fazer tarefa 2'},
-      { id: 3, title: 'Fazer tarefa 3'},
-      { id: 4, title: 'Fazer tarefa 4'},
-      { id: 5, title: 'Fazer tarefa 5'},
-      { id: 6, title: 'Fazer tarefa 6'},
-      { id: 7, title: 'Fazer tarefa 7'}
+      { id: 1, title: 'Training English later wake up'},
+      { id: 2, title: 'Pay the bill'},
+      { id: 3, title: 'Pay the net'},
+      { id: 4, title: 'Watching leason about Rails'},
+      { id: 5, title: 'Watching leason about Angular'},
+      { id: 6, title: 'Buy a pizza'},
+      { id: 7, title: 'Leave my daughter to school'}
     ];
     return { tasks }
   }
--- src/app/navbar/task-search/task-search.component.ts
+++ src/app/navbar/task-search/task-search.component.ts
@@ -1,13 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Task } from "src/app/tasks/shared/task.model";
 import { TaskService } from "src/app/tasks/shared/task.service";
 
+import { Observable, of, Subject } from "rxjs";
+
+import { switchMap} from 'rxjs/operators';
 @Component({
   selector: 'task-search',
   templateUrl: './task-search.html'
 })
 
-export class TaskSearchComponent {
+export class TaskSearchComponent implements OnInit{
+  public searchTerms: Subject<string> = new Subject();
+  public tasks: Task[] = [];
+
   public constructor(private taskService: TaskService){}
+
+  public ngOnInit() {
+    this.searchTerms.pipe(switchMap(
+      term => term ? this.taskService.searchByTitle(term) : Observable.of<Task[]>([]))
+    ).subscribe(tasks => console.log(tasks))
+  }
+
+  public search(term: string){
+    this.searchTerms.next(term);
+  }
 }
